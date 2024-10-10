28973c65e0ec95a562c1939d0d80935b4624335a
Emman <101505617+Bobblotto@users.noreply.github.com>
2024-10-10 18:28:58 +0100

    Add files via upload

--- changebackground/cbg.py
+++ changebackground/cbg.py
@@ -2,11 +2,11 @@ import cv2
 import numpy as np
 
 spongebob = cv2.imread("OpenCV2/changebackground/spongebob.jpg")
-spongebobhsv = cv2.cvtColor(spongebob, cv2.COLOR_BGR2HSV)
 bg = cv2.imread("OpenCV2/changebackground/bg.jpg")
 
-cv2.imshow("spongebob", spongebobhsv)
-cv2.waitKey(0)
+spongebob = cv2.resize(spongebob, (bg.shape[1], bg.shape[0]))
+
+spongebobhsv = cv2.cvtColor(spongebob, cv2.COLOR_BGR2HSV)
 
 lowergreen = np.array([45, 50, 50])
 highergreen = np.array([80, 255, 255])
@@ -19,9 +19,16 @@ print("non-green: ", nongreen)
 cv2.imshow("spongebob", nongreen)
 cv2.waitKey(0)
 
-#spongebobr = cv2.resize(spongebob, (750, 750))
-#bgr = cv2.resize(bg, (750, 750))
+spongebob = cv2.bitwise_and(spongebob, spongebob, mask=nongreen)
 
-#added = cv2.addWeighted(spongebob, 0.5, bg, 0.5, 0)
-#cv2.imshow("added img", added)
-#cv2.waitKey(0)
+cv2.imshow("win", spongebob)
+cv2.waitKey(0)
+
+bg = cv2.bitwise_and(bg, bg, mask=green)
+
+cv2.imshow("win", bg)
+cv2.waitKey(0)
+
+finished = cv2.addWeighted(spongebob, 1, bg, 1, 0)
+cv2.imshow("done", finished)
+cv2.waitKey(0)
---
+++ numlists.py
@@ -0,0 +1,8 @@
+
+
+list = [3, 5, 6, 8, 2, 3, 1, 4]
+
+for i in range(len(list)):
+    list[i] += 2
+
+print(list)
