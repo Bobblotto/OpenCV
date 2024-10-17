069f655b85769835588dd34a2eab8c3fa2fceda8
Emman <101505617+Bobblotto@users.noreply.github.com>
2024-10-17 18:48:53 +0100

    Add files via upload

--- inviscloak/inviscloak.py
+++ inviscloak/inviscloak.py
@@ -14,17 +14,24 @@ while True:
         frame = np.flip(frame, axis=1)
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         
-        lower_red = np.array([0, 120, 50])
-        upper_red = np.array([10, 255, 255])
+        lower_red = np.array([100, 40, 40])
+        upper_red = np.array([100, 255, 255])
         mask1 = cv2.inRange(hsv, lower_red, upper_red)
         
-        lower_red = np.array([170, 120, 70])
+        lower_red = np.array([155, 40, 40])
         upper_red = np.array([180, 255, 255])
         mask2 = cv2.inRange(hsv, lower_red, upper_red)
-
         mask3 = mask1 + mask2
-        print(mask3)
+        
+        red = cv2.morphologyEx(mask3, cv2.MORPH_OPEN, np.ones((3, 3), int), iterations=2)
+        red = cv2.dilate(red, np.ones((3, 3), int), iterations=1)
 
-        cv2.imshow("Video", frame)
+        nonred = cv2.bitwise_not(red)
+        invis = cv2.bitwise_and(bg, bg, mask=red)
+        visible = cv2.bitwise_and(frame, frame, mask=nonred)
+
+        finished = cv2.addWeighted(visible, 1, invis, 1, 0)
+
+        cv2.imshow("Video", finished)
     if cv2.waitKey(1) == 32:
         break
