1699854c4ef8519dff0c09a0c146acf2724c88df
Emman <101505617+Bobblotto@users.noreply.github.com>
2024-11-07 18:43:49 +0000

    Add files via upload

--- multicolourdetection/mcd.py
+++ multicolourdetection/mcd.py
@@ -29,9 +29,29 @@ while True:
     dilategreen = cv2.dilate(greenmask, np.ones((3, 3), int))
     green = cv2.bitwise_and(frame, frame, mask=dilategreen)
 
-    #cv2.findContours(red)
+    redcontours, n = cv2.findContours(dilatered, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    bluecontours, n = cv2.findContours(dilateblue, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    greencontours, n = cv2.findContours(dilategreen, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    cv2.imshow("video", red)
+    for index, contour in enumerate(redcontours):
+        area = cv2.contourArea(contour)
+        if area > 300:
+            x, y, width, height = cv2.boundingRect(contour)
+            cv2.rectangle(frame, (x, y), (x+width, y+height), (0, 0, 255), 2)
+
+    for index, contour in enumerate(bluecontours):
+        area = cv2.contourArea(contour)
+        if area > 300:
+            x, y, width, height = cv2.boundingRect(contour)
+            cv2.rectangle(frame, (x, y), (x+width, y+height), (255, 0, 0), 2)
+    
+    for index, contour in enumerate(greencontours):
+        area = cv2.contourArea(contour)
+        if area > 300:
+            x, y, width, height = cv2.boundingRect(contour)
+            cv2.rectangle(frame, (x, y), (x+width, y+height), (0, 255, 0), 2)
+
+    cv2.imshow("video", frame)
 
     if cv2.waitKey(1) == 32:
         break
